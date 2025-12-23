c5b598ca5bd022830039b2853e1390a9c7e14791
Nouf Alshawoosh <Noufshawoosh@gmail.com>
2025-12-23 11:29:41 +0300

    Initial Commit

---
+++ MovieIsMe_Team1_M/MovieIsMe_Team1_MApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieIsMe_Team1_MApp.swift
+//  MovieIsMe_Team1_M
+//
+//  Created by Nouf Alshawoosh on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MovieIsMe_Team1_MApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
