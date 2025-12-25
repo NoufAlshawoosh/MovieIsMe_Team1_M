f13f5abded3f0cedf36e9dfec60ea3b01467ad63
Nedaa <nedaatlou@gmail.com>
2025-12-25 18:18:34 +0300

    Movies Center

---
+++ MovieIsMe_Team1_M/View/MovieCenterView.swift
@@ -0,0 +1,19 @@
+//
+//  MovieCenterView.swift
+//  MovieIsMe_Team1_M
+//
+//  Created by Nedaa on 25/12/2025.
+//
+
+import SwiftUI
+
+struct MovieCenterView: View {
+    var body: some View {
+        ZStack {
+            
+            Text("هلا")
+                .font(.system(size: 28, weight: .bold))
+                .foregroundColor(.white)
+        }
+    }
+}
--- MovieIsMe_Team1_M/View/SignInView.swift
+++ MovieIsMe_Team1_M/View/SignInView.swift
@@ -3,5 +3,4 @@
 //  MovieIsMe_Team1_M
 //
 //  Created by Nedaa on 25/12/2025.
-//
 
