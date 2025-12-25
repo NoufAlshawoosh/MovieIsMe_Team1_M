c2f5355537177bb67da45a1d0f7f9f5a8cd22277
Nedaa <nedaatlou@gmail.com>
2025-12-25 18:56:38 +0300

    Sign Background

--- MovieIsMe_Team1_M/View/SignInView.swift
+++ MovieIsMe_Team1_M/View/SignInView.swift
@@ -4,3 +4,33 @@
 //
 //  Created by Nedaa on 25/12/2025.
 
+//
+//  SignInView.swift
+//
+//  Created by Nedaa on 24/12/2025.
+//
+import SwiftUI
+#Preview {
+    SignInView()
+}
+struct SignInView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
+
+    var body: some View {
+        ZStack {
+            Image("SignIn")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea()
+                .overlay(
+                    LinearGradient(
+                        colors: [.black.opacity(1.6), .clear],
+                        startPoint: .bottom,
+                        endPoint: .top
+                    )
+                )
+          
+        }
+    }
+}
